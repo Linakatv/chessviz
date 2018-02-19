0d1b300c777ebeabf4593aabc25e0ca2d4426b1d
Артём <arteym1234@mail.ru>
2018-02-19 23:35:34 +0700

    Good job

--- lina.c
+++ lina.c
@@ -5,12 +5,14 @@
 int main (int argc, char* argv[]){
 	int c;
 	FILE *file;
-	file = fopen(argv[1], "r");
+	file = fopen("word.txt", "r");
+
 	if (file) {
 		while ((c = getc(file)) != EOF)
 		putchar(c);
 		fclose(file);
 
 	}
+	printf("\n");
 	return 0;
 }
